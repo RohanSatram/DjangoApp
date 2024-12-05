f0ac28ceac47cea63d773126be8b2f2628731582
RohanSatram <satram.rohan@gmail.com>
2024-12-05 21:06:42 +0530

    Query2

--- django/polls/views.py
+++ django/polls/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 # Create your views here.
 
-def index(request):
-    query = request.GET.get('q')
-    if query == "toto":
+def index(request, UserName = None):
+    UserName = request.GET.get('q')
+    if UserName == "toto":
         return JsonResponse({"Type":"Human","Age":21,"IQ":130})
-    elif query == "nikki":
+    elif UserName == "nikki":
         return JsonResponse({"Type":"Human(Dumbass)","Age":30 ,"IQ":90})
+    else:
+        return HttpResponse("Username not found")
+    
